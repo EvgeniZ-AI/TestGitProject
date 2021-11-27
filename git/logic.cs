eb8f808fe9fb6b6b7750bb2613f3c4f49443d009
Женя Вито z z z <evgenizef.ai@gmail.com>
2021-11-27 19:29:07 +1000

    сделал функцию статической

--- git/logic.cs
+++ git/logic.cs
@@ -10,7 +10,7 @@ namespace git
     {
         public class log
         {
-            public int Power(int a,int p)
+            public static int Power(int a,int p)
             {
                 return (int)Math.Pow(a, p);
             }
